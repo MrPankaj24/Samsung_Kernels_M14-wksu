d0f56e49b8a4cfd05f02f3f646747e62f1b7fb05
jimsterino98 <n.n.hoxha@gmail.com>
2026-03-18 09:38:03 -0700

    Update build_kernel_GKI.sh
    added build_number

--- build_kernel_GKI.sh
+++ build_kernel_GKI.sh
@@ -7,6 +7,7 @@ export PROJECT_NAME=${MODEL}
 export REGION=$(echo ${BUILD_TARGET} | cut -d'_' -f2)
 export CARRIER=$(echo ${BUILD_TARGET} | cut -d'_' -f3)
 export TARGET_BUILD_VARIANT= user
+export BUILD_NUMBER=S916BXXU8EYI5
 		
 #2. sm8550 common config
  CHIPSET_NAME=kalama
