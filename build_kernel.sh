a956f408c16bb881f8eb2f506f20f1d5333e7d34
jimsterino98 <n.n.hoxha@gmail.com>
2026-03-20 00:26:01 -0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -16,4 +16,16 @@ export ARCH=arm64
 export TARGET_SOC=s5e8835
 export BUILD_NUMBER=A546EXXUCDYDB
 make s5e8835-a54xnsxx_defconfig
+
+if [ "$KPM" = "1" ]; then
+    echo "[+] KPM build: disabling RWX protections"
+    scripts/config --disable STRICT_KERNEL_RWX
+    scripts/config --disable STRICT_MODULE_RWX
+else
+    echo "[+] Normal build: keeping RWX protections enabled"
+fi
+
+# Debug check
+grep RWX .config
+
 make
