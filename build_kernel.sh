ae636dfd4fa6f5459399e948eb75b039168917b7
jimsterino98 <n.n.hoxha@gmail.com>
2026-03-20 19:19:41 -0700

    Update build_kernel.sh
    added more disables

--- build_kernel.sh
+++ build_kernel.sh
@@ -19,6 +19,8 @@ make s5e8835-a54xnsxx_defconfig
 
 if [ "$KPM" = "1" ]; then
     echo "[+] KPM build: disabling RWX protections"
+    scripts/config --disable ARCH_HAS_STRICT_MODULE_RWX
+    scripts/config --disable ARCH_HAS_STRICT_KERNEL_RWX
     scripts/config --disable STRICT_KERNEL_RWX
     scripts/config --disable STRICT_MODULE_RWX
 else
